2603a7f984150d7fa62ee550c39e547f2e8c4bb9
Tim Danner <tim.danner@solarwinds.com>
2020-05-01 11:35:56 -0500

    Add env vars for go

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,5 @@
 export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export PATH=/usr/local/mysql/bin:/usr/local/bin:$PATH
+export PATH=/usr/local/go/bin:$PATH
+export GOROOT=/usr/local/go
